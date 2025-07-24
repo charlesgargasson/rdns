30e456d7e83ffbf50af9fac84dfeb8726fe15bb7
charles <charles>
2025-07-24 17:04:30 +0200

    fix build options

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd -- "$(dirname -- "$0")"
-VCS="-buildvcs=false"
 go version
 go get main
 set -x
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildmode=pie -o bin/rdns $VCS &
-env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 CC=x86_64-w64-mingw32-gcc go build -buildmode=pie -o "bin/rdns.exe" $VCS &
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -buildmode=pie -o "bin/rdns" -a -ldflags '-s -w -extldflags "--static-pie"' -buildvcs=false -tags 'osusergo,netgo,static_build' &
+env GOOS=windows GOARCH=amd64 CGO_ENABLED=0 CC=x86_64-w64-mingw32-gcc go build -buildmode=pie -o "bin/rdns.exe" -a -ldflags '-s -w -extldflags "--static-pie"' -buildvcs=false -tags 'osusergo,netgo,static_build' &
 wait
